fc4e428ae85a3f0a2d3d155a226c0f5faf2b1bfc
Cesar Augusto <cesinhagutierres@gmail.com>
2020-04-19 13:13:52 -0300

    estratégia de cache para o repositorio

--- migrations/2020_04_19_000000_create_options_table.php
+++ migrations/2020_04_19_000000_create_options_table.php
@@ -15,12 +15,10 @@ class CreateOptionsTable extends Migration
     {
         Schema::create('options', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 35);
-            $table->string('label', 35);
-            $table->tinyInteger('action')->default(1);
+            $table->string('label', 60);
+            $table->string('action', 60)->index();
             $table->text('description')->nullable();
-            $table->boolean('default_status')->default(true);
-            $table->unique(['name','label']);
+            $table->boolean('default_status');
             $table->timestamps();
         });
     }
--- src/Repositories/OptionableRepository.php
+++ src/Repositories/OptionableRepository.php
@@ -9,17 +9,96 @@ use Plug2Team\Optionable\Models\Option;
 
 class OptionableRepository
 {
-    protected ?Model $model;
+    protected ?Option $model;
 
+    /**
+     * @param Option $model
+     */
     public function __construct(Option $model)
     {
         $this->model = $model;
     }
 
-    public function all()
+    /**
+     * @param string $label
+     * @param string $action
+     * @param string|null $description
+     * @return mixed
+     */
+    public function insert(string $label, string $action, ?string $description = null)
     {
-        dd($this->resolveCache());
-        return $this->model->all();
+        $option = $this->model->newQuery()->firstOrNew(compact('label'));
+        $option->action = $action;
+        $option->description = $description;
+        $option->default_status = config('optionable.option.default_value');
+        $saved = $option->save();
+
+        if($saved) {
+            $this->persistItem($option);
+        }
+
+        return $saved;
+    }
+
+    /**
+     * Retrieve all
+     *
+     * @return mixed
+     */
+    public function all() : array
+    {
+        $items = [];
+
+        foreach ($this->getIndexs() as $index) {
+            $cache = $this->getCacheName($index);
+            $items[] = $this->resolveCache()->get($cache);
+        }
+
+        if(!$items) {
+            $this->model->all()->each(fn($h) => $this->persistItem($h));
+            return $this->all();
+        }
+
+        return $items;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function addIndex(int $id)
+    {
+        $index = $this->getIndexs();
+        array_push($index, $id);
+        $this->resolveCache()->put('index', $index);
+    }
+
+    /**
+     * @return array
+     */
+    public function getIndexs(): array
+    {
+        return  $this->resolveCache()->get("index") ?? [];
+    }
+
+    /**
+     * @param $item
+     * @return void
+     */
+    public function persistItem($item) : void
+    {
+        $this->addIndex($item->id);
+
+        // persist option
+        $this->resolveCache()->add($this->getCacheName($item->id), $item, config('optionable.cache.ttl'));
+    }
+
+    /**
+     * @param mixed ...$args
+     * @return string
+     */
+    private function getCacheName(...$args)
+    {
+        return sprintf(config('optionable.cache.name'), ...$args);
     }
 
     /**
@@ -27,6 +106,6 @@ class OptionableRepository
      */
     private function resolveCache()
     {
-        return app('cache')->tags(config('optionable.cache_tag_name'));
+        return app('cache')->tags(config('optionable.cache.tag'));
     }
 }
